f6d91c9c5015a0dd3502563758a91693de45ce06
Gustavo Coronel <gcoronelc@gmail.com>
2017-05-24 12:01:16 -0500

    Sem 10

--- Sem10/EurekaSpring/src/java/pe/egcc/eureka/controller/AppController.java
+++ Sem10/EurekaSpring/src/java/pe/egcc/eureka/controller/AppController.java
@@ -1,14 +1,21 @@
 package pe.egcc.eureka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pe.egcc.eureka.service.CuentaService;
 
 @Controller
 public class AppController {
+  
+  @Autowired
+  private CuentaService cuentaService;
 
   @RequestMapping(value = "index.htm", method = RequestMethod.GET)
-  public String home(){
+  public String home(Model model){
+    model.addAttribute("cantCuentas", cuentaService.getCantCuentas());
     return "index";
   }
 
